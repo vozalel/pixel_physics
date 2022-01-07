7deda024d192972cc50c613067bee17735d65374
vozalel <vozalel@gmail.com>
2022-01-07 14:16:40 +0500

    первая абсолютно работающая версия для одного шарика

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ pygame.init()
 
 WIDTH = 1000
 HEIGHT = 1000
-SOFTNESS = 0.04
+SOFTNESS = 0.5
 
 GRAVITY = 0.9
 # INCLINE - наклон получаемый с гироскопа, здесь относительный в долях единицы
 # от -1: наклон правой/нижней стороной вверх до +1 наклон левой/верхней стороной вверх
 # если 0 то горизонтально поверхности земли
 INCLINE_X = 0
-INCLINE_Y = 0.001
+INCLINE_Y = 0.5
 COLOUR = (180, 180, 180)
 SIZE = 10
 
@@ -25,8 +25,8 @@ class Ball:
         self.direction = direction
         self.t = 0
 
-    def move(self):
-        self.position = self.get_next_position()
+    def move(self, inclane):
+        self.position = self.get_next_position(inclane)
         self.t += 0.001
 
     def show(self, screen):
@@ -39,28 +39,27 @@ class Ball:
 
     def get_increment(self):
         dx, dy = self.direction
-        #dx = GRAVITY * self.t
+        dx = dx + GRAVITY * self.t
         dy = dy + GRAVITY * self.t
         return (dx, dy)
 
-    def get_next_position(self):
+    def get_next_position(self, inclane):
         x, y = self.position
         dx, dy = self.get_increment()
-        print(dx, dy)
+        dy *= inclane['y']
+        dx *= inclane['x']
         return (x + dx, y + dy)
 
-    def collision(self):
-        x, y = self.get_next_position()
+    def collision(self, inclane):
+        x, y = self.get_next_position(inclane)
         if SIZE > x or x > WIDTH - SIZE:
             dx, dy = self.get_increment()
-            self.direction = (0, 0)
-            # self.direction = (-dx, dy)
+            self.direction = (-dx * SOFTNESS, dy)
             self.t = 0
             return True
         if SIZE > y or y > HEIGHT - SIZE:
             dx, dy = self.get_increment()
-            self.direction = (0, 0)
-            # self.direction = (dx, -dy)
+            self.direction = (dx, -dy * SOFTNESS)
             self.t = 0
             return True
         return False
@@ -71,13 +70,13 @@ class Ball:
 
 
 x = WIDTH / 2 # random.randint(SIZE, WIDTH - SIZE)
-y = HEIGHT - SIZE # random.randint(SIZE, HEIGHT - SIZE)
+y = HEIGHT / 2 # random.randint(SIZE, HEIGHT - SIZE)
 dx2 = 0 # random.random()
 dy2 = 0
 
 ball = Ball(
     (x, y),
-    (0, -0.6)
+    (0, 0)
 )
 
 
@@ -94,11 +93,13 @@ commands_move = {
     pygame.K_RIGHT: 'RIGHT'
 }
 
-def update_inclane(command):
-    if command == 'UP': pass
-    elif command == 'DOWN': pass
-    elif command == 'LEFT': pass
-    elif command == 'RIGHT': pass
+def update_inclane(command, inclane):
+    pass
+
+inclane = {
+    'x': 0,
+    'y': 1,
+}
 
 while not GAME_END:
     game_screen.fill((0,0,0))
@@ -114,12 +115,19 @@ while not GAME_END:
 
     if not pause:
         for command in (commands_move[key] for key in commands_move if pressed[key]):
-            update_inclane(command)
-            print(command)
-        if ball.collision():
-            pause = not pause
-            print (ball.t, ball.direction)
-        ball.move()
+            if command == 'UP' and inclane['y'] < 1:
+                inclane['y'] += 0.001
+            elif command == 'DOWN' and inclane['y'] > -1:
+                inclane['y'] -= 0.001
+            if command == 'LEFT' and inclane['x'] < 1:
+                inclane['x'] += 0.001
+            elif command == 'RIGHT' and inclane['x'] > -1:
+                inclane['x'] -= 0.001
+            print(inclane)
+
+        ball.collision(inclane)
+
+        ball.move(inclane)
 
     ball.show(game_screen)
     pygame.display.update()
